26d9f37ff828f27532abe349018c23ffa0f3ce09
Nguyen Vu <85844805+maxlol1102@users.noreply.github.com>
2022-08-01 23:01:39 +0700

    sửa layout và làm phần liên hệ customer
    xong

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,5 @@
  @extends('admin_layout')     {{-- ke thua lai cua trang admin_layout --}}  {{-- ket noi vs adminlayout --}}
  @section('admin_content')		{{-- noi nao nhan trang nay thi goi ra --}}
- <h3><font color="white">Chào mừng bạn đến với trang quản lý MY-COMPUTER</font></h3>
+ <h3 style="text-align: center; font-family: 'Open Sans'; font-size: 30px"><font color="white">Chào mừng bạn đến với trang quản lý MY-COMPUTER</font></h3>
+
  @endsection
